75f5694dc388e82504d17c9dc33b0011eae1c226
Wes Baker <wcbaker@gmail.com>
2010-05-26 15:46:02 -0400

    Modified version number.

--- extensions/ext.wb_freeform_campaign_monitor.php
+++ extensions/ext.wb_freeform_campaign_monitor.php
@@ -7,7 +7,7 @@ class Wb_freeform_campaign_monitor
 	
 	var $name           = 'WB Freeform Campaign Monitor';
 	var $class_name     = 'Wb_freeform_campaign_monitor';
-	var $version        = '1.0';
+	var $version        = '1.0.1';
 	var $description    = 'After receiving a form entry, sends it to Campaign Monitor';
 	var $settings_exist = 'y';
 	var $docs_url       = '';
